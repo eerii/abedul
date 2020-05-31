015be8cdd1c77094d716d38246a1bdac7ea50021
josekoalas <jose.pazos.perez@rai.usc.es>
2020-05-31 21:24:56 +0200

    2.13 Country Data

--- part2/countrydata/src/index.js
+++ part2/countrydata/src/index.js
@@ -1,11 +1,20 @@
-import React, {useEffect, useState} from 'react';
-import ReactDOM from 'react-dom';
-import axios from "axios";
+import React, {useEffect, useState} from 'react'
+import ReactDOM from 'react-dom'
+import axios from "axios"
+import './index.css'
 
-const Display = ({name}) => <p>{name}</p>
+const Button = ({onClick, name}) => <button onClick={onClick}>{name}</button>
+
+const ListDisplay = ({name, setSearchVal}) => {
+    return (
+        <>
+            <p>{name} <Button name="View" onClick={event => setSearchVal(name)}/></p>
+        </>
+    )
+}
 
 const CountryDisplay = (props) => {
-    const country = props.props
+    const country = props.country
     return (
         <>
             <h1>{country.name}</h1>
@@ -15,22 +24,22 @@ const CountryDisplay = (props) => {
             <ul>
                 {country.languages.map(lan => <li key={lan.name}>{lan.name}</li>)}
             </ul>
-            <img src={country.flag} alt={"Flag"}></img>
+            <img src={country.flag} alt="Flag" id="flag"></img>
         </>
     )
 }
 
-const Countries = ({countries}) => {
+const Countries = ({countries, setSearchVal}) => {
     let list = ""
     if (countries.length > 10) {
         list = "There are too many countries that match your search, please provide further details"
     }
     else if (countries.length > 1) {
-        list = countries.map(countries => <Display key={countries.name} name={countries.name}/>)
+        list = countries.map(countries => <ListDisplay key={countries.name} name={countries.name} setSearchVal={setSearchVal}/>)
     }
     else if (countries.length === 1) {
         const [country] = countries
-        list = <CountryDisplay props={country}/>
+        list = <CountryDisplay country={country}/>
     }
 
     return (
@@ -60,7 +69,7 @@ const App = () => {
         <div>
             <h1>Country Data</h1>
             Search: <input value={searchVal} onChange={event => setSearchVal(event.target.value)}/>
-            <Countries countries={searchCountries}/>
+            <Countries countries={searchCountries} setSearchVal={setSearchVal}/>
         </div>
     )
 }
